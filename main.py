d679abc762bca9c23c65f726e5bea41f979c8321
sarahgar1and <149905696+sarahgar1and@users.noreply.github.com>
2023-11-21 23:00:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -38,48 +38,110 @@ class Frog():
         i = self.counter
         if self.isMoving:
             image = self.walking[i % len(self.walking)]
-            imageWidth, imageHeight = image.width, image.height
             if self.direction == 'right':
                 image = image.transpose(Image.FLIP_LEFT_RIGHT)
+            newWidth, newHeight = getNewDims(image, 5)
             image = CMUImage(image)
-            newWidth, newHeight = imageWidth//5, imageHeight//5
             drawImage(image, self.x, self.y,align='center',
                     width=newWidth,height=newHeight)
         else:
             imageWidth, imageHeight = self.standing.width, self.standing.height
-            newWidth, newHeight = imageWidth//5, imageHeight//5
+            newWidth, newHeight = getNewDims(self.standing, 5)
             image = CMUImage(self.standing)
             drawImage(image, self.x, self.y,align='center',
                     width=newWidth, height=newHeight)
             
 class Plant():
+    harvestTimes = {}
+    plantImages = {}
     def __init__(self, species):
         self.species = species
     # 4 stages: seed, baby, adolescent, adult  
-        self.stage = 0 
-        self.watered = False
+        self.stage = 'seed' 
+        self.image = Image.open("images/seed.png")
     # harvestable once daysTillHarvest == harvestTime
-    # do not progess if not watered
-        self.daysTillHarvest = 0 
+        self.days = 0 
 
-    def harvestTime(self):
-        pass
-    def agingTime(sef):
-        pass
-    def plantImages(self):
-        pass
+    def grow(self): # call when watered!!
+        self.days += 1
+        if self.days == Plant.harvestTimes[self.species]:
+            self.stage = 'ready!'
+        if self.days == 1:
+            self.image = Image.open("images/sapling.png")
+        elif self.days == 2:
+            self.image = Plant.plantImages[f'{self.species}-0']
+        elif self.days == 4: # spend 2 days as an adolescent
+            self.image = Plant.plantImages[f'{self.species}-1']
+    
+    def draw(self):
+        image = CMUImage(self.image)
+        drawImage(image, self.x, self.y)
+            
 
-def drawMap(app):
+class Button():
+    buttonImages = {'play': Image.open("images/play.png"),
+                    'about': Image.open("images/play.png")}
+    buttonPos = {'play': (175, 200),
+                 'about': (175,300)}
+    def __init__(self, task):
+        self.task = task
+        self.image = Button.buttonImages[task]
+        self.x, self.y = Button.buttonPos[task]
+    
+    def __repr__(self):
+        return f'{self.task}'
+
+    def draw(self):
+        image = self.image
+        self.width, self.height = getNewDims(image, 6)
+        image = CMUImage(image)
+        drawImage(image, self.x, self.y,
+                  width=self.width, height=self.height) # don't align center
+
+def getNewDims(image, factor):
+    width,height = image.width, image.height
+    return width/factor, height/factor 
+
+#------------------------------------------Screens Functions
+def drawFarm(app):
     pass
+
+def drawStartScreen(app):
+    screen = Image.open("images/home.png")
+    newWidth, newHeight = getNewDims(screen, 2.5)
+    screen = CMUImage(screen)
+    drawImage(screen, app.width/2, app.height/2, 
+              align='center', width=newWidth, height=newHeight)
+    app.play.draw()
+    app.about.draw()
+
+def drawAboutScreen(app):
+    pass
+
+#------------------------------------------
 
 def onAppStart(app):
     app.stepsPerSecond = 10
+# frog :3
     app.frog = Frog(app)
     app.frog.isMoving = False
     app.frog.counter = 0
 
+    app.screen = 'start'
+# buttons
+    app.play = Button('play')
+    app.about = Button('about')
+    app.buttonsList = [app.play, app.about]
+
 def redrawAll(app):
-    drawMap(app)
+    if app.screen == 'start':
+        drawStartScreen(app)
+        return
+    elif app.screen == 'about':
+        drawAboutScreen(app)
+        return
+    elif app.screen == 'farm':
+        drawFarm(app)
     app.frog.draw()
 
 def onKeyPress(app, key):
@@ -88,15 +150,34 @@ def onKeyPress(app, key):
         app.frog.isMoving = True
         app.frog.takeStep()
 
+def onKeyRelease(app, key):
+    app.frog.isMoving = False
+
+def onMousePress(app, mouseX, mouseY):
+    button = getButtonClicked(app, mouseX, mouseY)
+    print(button)
+    if button != None:
+        app.screen = button.task
+        print(app.screen)
+
+def getButtonClicked(app, mx, my):
+    for button in app.buttonsList:
+        if (button.x < mx < (button.x + button.width) 
+            and (button.y + button.height) < button.y < my):
+            # re-center frog
+            app.frog.x, app.frog.y = app.width/2, app.height/2
+            return button
+    return None
+
+def distance(x0, y0, x1, y1):
+    return ((x0-x1)**2 + (y0-y1)**2)**0.5
+
 def onStep(app):
     if app.frog.isMoving:
         app.frog.takeStep()
         app.frog.counter += 1
 
-def onKeyRelease(app, key):
-    app.frog.isMoving = False
-
 def main():
-    runApp(1000, 700)
+    runApp(944, 656)
 
 main()
