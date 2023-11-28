8143c8cc2a154322172d8c69829c1c7c28c12ffa
sarahgar1and <149905696+sarahgar1and@users.noreply.github.com>
2023-11-28 17:51:50 -0500

    Add files via upload

--- mapGeneration.py
+++ mapGeneration.py
@@ -1,48 +1,94 @@
 from cmu_graphics import *
-import random
+import random, copy
 
 def onAppStart(app):
-    app.forestSize = 41
-    print(forest_createMap(app))
+    app.forestSize = 9 #must be odd and able to be odd when quartered
+    forest_createMap(app)
+    for L in app.forestMap: print(L)
 
 def forest_createMap(app): 
     #diamond square algorithm source: 
     #https://learn.64bitdragon.com/articles/computer-science/procedural-generation/the-diamond-square-algorithm 
     app.forestMap = [[0]*app.forestSize for i in range(app.forestSize)]
-    
+    randomness = 183
     #step 1. set corners to same random value
-    randomValue = random.randint(0, 200)
+    randomValue = random.randint(1, 200)
     app.forestMap[0][0] = randomValue
     app.forestMap[0][app.forestSize-1] = randomValue
-    app.forestMap[app.forestSize - 1][0] = randomValue
+    app.forestMap[app.forestSize-1][0] = randomValue
     app.forestMap[app.forestSize-1][app.forestSize-1] = randomValue
+    return forest_createMapHelper(app, app.forestMap, randomness)
 
-    #step 2. set center index to average of conrners + a random displacement
-    randomDisplacement = random.randint(0, 10)
-    centerIndex = (app.forestSize-1)//2
-    app.forestMap[centerIndex][centerIndex] = randomValue + randomDisplacement
+def forest_createMapHelper(app, currMap, randomness):
+    if len(currMap[0])<3: return
+    else:
+        for L in currMap: print(L)
+        print('Before mod \n')
+        randomDisplacement = random.randint(1, randomness)
+        currMapEdge = len(currMap[0])-1
+        #step 2. set center index to average of conrners +
+        #  a random displacement
+        centerIndex = (currMapEdge)//2
+        sumOfCorners = ((((currMap[0][0] + currMap[0][currMapEdge] + 
+                          currMap[currMapEdge][0] + 
+                          currMap[currMapEdge][currMapEdge])//4)))
+        
+        currMap[centerIndex][centerIndex] = sumOfCorners + randomDisplacement
 
-    #step 3. set midpoints of edges to average of corners + a random displacement
-    rand = random.randint(0, 10)
-    #top point:
-    app.forestMap[0][centerIndex] = ((((app.forestMap[0][0] + 
-                                       app.forestMap[0][app.forestSize-1])//2 + 
-                                       rand)))
-    #bottom point: (same as top)
-    app.forestMap[0][centerIndex] = ((((app.forestMap[0][0] + 
-                                       app.forestMap[0][app.forestSize-1])//2 + 
-                                       rand)))
-    rand = random.randint(0, 10)
-    #left point:
-    app.forestMap[centerIndex][0] = ((((app.forestMap[0][0] + 
-                                       app.forestMap[app.forestSize-1][0])//2 + 
-                                       rand)))
-    #right point: (same as left)
-    app.forestMap[centerIndex][0] = ((((app.forestMap[0][0] + 
-                                       app.forestMap[app.forestSize-1][0])//2 + 
-                                       rand)))
-    #step 4. recurse and decrease randomDisplacement
-    return app.forestMap
+        #step 3. set midpoints of edges to average of corners +
+        #a random displacement
+        rand = random.randint(0, 10)
+        #top point:
+        currMap[0][centerIndex] = ((((currMap[0][0] + 
+                                            currMap[0][currMapEdge])//2 + 
+                                            rand)))
+        #bottom point: 
+        currMap[currMapEdge][centerIndex] = ((((currMap[0][currMapEdge] + 
+                                            currMap[currMapEdge][currMapEdge])//2 + 
+                                            rand)))
+        rand = random.randint(0, 10)
+        #left point:
+        currMap[centerIndex][0] = ((((currMap[0][0] + 
+                                            currMap[currMapEdge][0])//2 + 
+                                            rand)))
+        #right point: 
+        currMap[centerIndex][currMapEdge] = ((((currMap[currMapEdge][0] + 
+                                            currMap[currMapEdge][currMapEdge])//2 + 
+                                            rand)))
+
+
+        for L in currMap: print(L)
+        print('After mod \n')
+        #ADD THIS TO INTO app.forestMap AT CORRECT IDENCIES
+
+        #step 4. recurse and decrease randomDisplacement
+        #to recurse: we need to chop current map into 4 smaller maps
+
+        newTopLefts = [(0, 0), (0, centerIndex), (centerIndex, 0), 
+                       (centerIndex, centerIndex)]
+        newDim = (len(currMap)+1)//2
+        currMapCopy = copy.deepcopy(currMap)
+        for x, y in newTopLefts:
+            newMap = [[0]*newDim for i in range(newDim)]
+            for row in range(newDim):
+                for col in range(newDim):
+                    newMap[row][col] = currMapCopy[row+x][col+y]
+            forest_createMapHelper(app, newMap, randomness//2 +2)
+            addToCurrMap(app, currMap, newMap, x, y)
+
+def addToCurrMap(app, currMap, subMap, topLeftX, topLeftY):
+    subMapEdge = len(subMap) -1
+    oldCenter = subMapEdge//2
+    newCenter = (topLeftX + topLeftX + subMapEdge)//2 #topLeft + topRight //2
+    #topLeftX and topLeftY are original position in map we quartered
+    newBottomX = topLeftX + subMapEdge
+    newBottomY = topLeftY + subMapEdge
+
+    currMap[topLeftX][newCenter] = subMap[0][oldCenter]
+    currMap[newCenter][topLeftY] = subMap[oldCenter][0]
+    currMap[newCenter][newBottomX] = subMap[oldCenter][subMapEdge]
+    currMap[newBottomY][newCenter] = subMap[subMapEdge][oldCenter]
+
 
 def main():
     runApp()
