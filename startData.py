51ac1a95634b7273049d2464c7b571c17ff9af3b
sarahgar1and <149905696+sarahgar1and@users.noreply.github.com>
2023-12-03 20:39:42 -0500

    Add files via upload

--- startData.py
+++ startData.py
@@ -1,40 +1,90 @@
+#data csv file:
+    #1. day
+    #2. frog hunger
+    #3. frog sleep
+    #4. money
+    #5. inventory list
+    #6. num in inventory
+    #7. dirt cells
+    #8. plants
+    #9. plants days alive
+    #10. plants location
+
+#------------------------------------------LOAD
 def loadStartFile(app):
-    i = 1
-    #1 day
-    #2 frog hunger
-    #3 frog sleep
-    #4 money
-    #5 inventory list
-    #6 num in inventory
-    #7 dirt cells
-    #8 plants
-    #9 plants days alive
-    #10 plants location
+    dataList = []
     with open('data.csv', 'r') as file:
         data = file.read()
-    # print(data)
     for line in data.splitlines():
-        if i == 1:
-            app.day = int(line)
-        elif i == 2:
-            app.frog.hunger = int(line)
-        elif i == 3:
-            app.frog.sleep = int(line)
-        elif i == 4:
-            app.money = int(line)
-        # elif i == 5:
-        #     editInventory(app, line)
-        # elif i == 6:
-        #     editInventoryNums(app, line)
-        # elif i == 7:
-        #     editDirtCells(app, line)
-        # elif i == 8:
-        #     plantsString = line
-        # elif i == 9:
+         dataList.append([line])
+    # print(dataList)
+#integers:
+    app.day = int(dataList[0][0])
+    app.frog.hunger = int(dataList[1][0])
+    app.frog.sleep = int(dataList[2][0])
+    app.money = int(dataList[3][0])
+#inventory:
+    if dataList[4][0] != '':
+        loadInventory(app, dataList[4][0], dataList[5][0])
+#dirt cells:
+    if dataList[6][0] != '':
+        loadDirt(app, dataList[6][0])
+#plants:
+    if dataList[7][0] != '':
+        loadPlants(app, dataList[7][0], dataList[8][0], dataList[9][0])
 
-        i +=1
+def loadInventory(app, itemString, numString):
+    #itemString looks like: 'tomato blueberrySeed strawberry'
+    #numString looks like: '1 1 2'
+    numList = []
+    for num in numString.split(' '):
+        numList.append(int(num))
 
-         
+    i=0 #keep track of place in string, index into numList
+    for item in itemString.split(' '):
+        if 'Seed' in item:
+            item = item[:-4] #get rid of 'Seed'
+            seed = Seed(item)
+            seed.num = numList[i]
+            addToInventory(app, seed)
+        else: 
+            crop = Crop(item)
+            crop.num = numList[i]
+            addToInventory(app, crop)
+        i += 1
+
+def loadDirt(app, cellString):
+    #cellString looks like: '100,300 400,500 600,200'
+    for line in cellString.split(' '):
+        cellLeft, cellTop = line.split(',')
+        app.dirtCells.add((int(cellLeft), int(cellTop)))
+
+        x, y = int(cellLeft)+app.cellWidth/2, int(cellTop)+app.cellHeight/2
+        app.plants[(x, y)] = 'dirt' #can put something here
+
+def loadPlants(app, plantString, dayString, locationString):
+    # plantString looks like: 'tomato strawberry wheat'
+    # dayString looks like: '1 2 4'
+    # locationString looks like: '150.0,128.0 400.0,500.0'
+    dayList = []
+    for day in dayString.split(' '):
+        dayList.append(int(day))
+
+    plantList = []
+    i = 0
+    for item in plantString.split(' '):
+        plant = Plant(item)
+        plant.days = dayList[i]
+        plantList.append[plant]
+        i += 1
+
+    i = 0
+    for coords in locationString.split(' '):
+        x, y = coords.split(',')
+        app.plants[(int(x), int(y))] = plantList[i]
+        i += 1
+
+#------------------------------------------SAVE     
 def saveData(app):
     #in order of lines:
     inventoryItems = getInventoryItems(app)
@@ -44,7 +94,7 @@ def saveData(app):
     plantsDays = getPlantDays(app)
     plantsLocations = getPlantLocations(app)
     with open('data.csv', 'w') as file:
-        file.write(f"{app.day}\n{app.frog.hunger}\n{app.frog.sleep}\n{app.money}\n{inventoryItems}\n{numInventoryItems}\n{dirtCells}\n{plants}\n{plantsDays}\n{plantsLocations}") #this rewrites the entire thing
+        file.write(f"{app.day}\n{app.frog.hunger}\n{app.frog.sleep}\n{app.money}\n{inventoryItems}\n{numInventoryItems}\n{dirtCells}\n{plants}\n{plantsDays}\n{plantsLocations}")
 
 def getInventoryItems(app):
     itemString = ''
@@ -91,5 +141,3 @@ def getPlantLocations(app):
             x, y = cell
             locations += f' {x},{y}'
     return locations.strip()
-
-
